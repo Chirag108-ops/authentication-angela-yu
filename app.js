ae33d1431cfb26c6243339792b17a03f53a04cbb
Chirag Goyal <chiraggoyal@Chirags-MacBook-Air.local>
2022-08-22 14:24:40 +0530

    Used Password Authentication

--- app.js
+++ app.js
@@ -2,20 +2,35 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
 const ejs = require('ejs')
 app.use(express.static('public'))
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended : true}))
-const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption')
+app.use(session({
+  secret : 'Our little secret.',
+  resave : false,
+  saveUninitialized : false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 mongoose.connect('mongodb://localhost:27017/userDB')
 
 const userSchema = new mongoose.Schema({
   email : String,
   password : String
 })
-userSchema.plugin(encrypt,{secret : process.env.SECRET, encryptedFields : ['password']})
+userSchema.plugin(passportLocalMongoose)
 const User = new mongoose.model('User',userSchema)
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.get('/',(req,res) => {
   res.render('home')
@@ -28,38 +43,50 @@ app.get('/login',(req,res) => {
 app.get('/register',(req,res) => {
   res.render('register')
 })
-
-app.post('/register', (req,res) => {
-  const newUser = new User({
-    email : req.body.username,
-    password : req.body.password
-  })
-  newUser.save((err) => {
+app.get('/secrets',(req,res) => {
+  if(req.isAuthenticated()){
+    res.render('secrets')
+  } else {
+    res.redirect('/login')
+  }
+})
+app.get('/logout', (req,res) => {
+  req.logout((err) => {
     if(err){
       console.log(err)
     } else {
-      res.render('secrets')
+      res.redirect('/')
+    }
+  })
+})
+app.post('/register', (req,res) => {
+  User.register({username: req.body.username}, req.body.password, (err,user) => {
+    if(err){
+      console.log(err)
+      res.redirect('/register')
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secrets')
+      })
     }
   })
 })
 
 app.post('/login', (req,res) => {
-  const username = req.body.username
-  const password = req.body.password
-  User.findOne({email: username}, (err,foundUser) => {
+  const user = new User({
+    username : req.body.username,
+    password : req.body.password
+  })
+  req.login(user,(err) => {
     if(err){
       console.log(err)
     } else {
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render('secrets')
-        }
-      }
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secrets')
+      })
     }
   })
 })
-
-
 
 app.listen(3000, () => {
   console.log('Server started on port 3000')
